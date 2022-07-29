1394d0f80d2e6cf7701c434f09cce23265a3ebeb
Thomas Chardonnens <chardonnens.thomas@protonmail.com>
2022-07-29 02:59:14 +0200

    New front for Gotgot project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'search', component: SearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
